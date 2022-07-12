293567255be0ac32229414763ef80b402a43ae68
udayabhanu2606 <109141968+udayabhanu2606@users.noreply.github.com>
2022-07-12 15:36:40 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
---
+++ lab4.py
@@ -0,0 +1,27 @@
+myFruitList = ["apple", "banana", "cherry",""]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+print(myFruitList[3])
+myFruitList[3] = "orange"
+print(myFruitList)
+myFruitList[2] = "kiwi"
+myFinalAnswerTuple = ("apple", "banana", "pineapple", "orange")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+print(myFinalAnswerTuple[3])
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
